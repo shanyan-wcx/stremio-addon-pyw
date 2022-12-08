3dd9b5254bf6480ba20ed2458a4da344ca140eb5
shanyan-wcx <2163304747@qq.com>
2022-12-08 16:41:04 +0800

    update

--- server.js
+++ server.js
@@ -3,4 +3,4 @@
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 49595 })
-// publishToCentral("https://my-addon.awesome/manifest.json")
+publishToCentral("https://2b8facee70bc-pyw.baby-beamup.club/manifest.json")
